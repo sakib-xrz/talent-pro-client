7deac8b573c15df1794eeb432ca96c91413b2e83
sakib-repliq <sakibul.islam@repliq.dev>
2023-11-01 02:19:27 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        base: "#F9FAFB",
+        error: "#E30909",
+        primary: "#65a30d",
+        "primary-200": "#d9f99d",
+        "primary-100": "#ecfccb",
+        "grey-800": "#1F2937",
+        "grey-500": "#6B7280",
+        "grey-400": "#9CA3AF",
+        "grey-300": "#D1D5DB",
+        "grey-200": "#E5E7EB",
+        "grey-100": "#F3F4F6",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
