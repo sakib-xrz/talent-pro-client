3717b96da1cededaf922c96045f1475aa3dc9f50
sakib-xrz <sakibxrz21@gmail.com>
2023-12-31 22:25:33 +0600

    recruiter job details page created

---
+++ app/recruiter/jobs/[uid]/page.jsx
@@ -0,0 +1,9 @@
+import Container from "@/components/shared/Container";
+
+export default function RecruiterJobDetailsPage() {
+  return (
+    <Container>
+      <p>Job details page coming soon...</p>
+    </Container>
+  );
+}
