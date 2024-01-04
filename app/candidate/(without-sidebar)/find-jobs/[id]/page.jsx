77c6d6249f375d5d0194ab0fe6b133ed72c663f6
sakib-repliq <sakibul.islam@repliq.dev>
2024-01-04 13:29:06 +0600

    info text size fix

--- app/candidate/(without-sidebar)/find-jobs/[id]/page.jsx
+++ app/candidate/(without-sidebar)/find-jobs/[id]/page.jsx
@@ -23,22 +23,26 @@ export default function CandidateJobDetails({ id }) {
           <div className="flex flex-col-reverse items-center justify-between gap-4 md:flex-row md:gap-8 ">
             <div className="flex w-full flex-col gap-3 text-center md:gap-6 md:text-left">
               <div className="space-y-3 text-primary">
-                <p className="text-2xl font-semibold md:text-4xl">
+                <p className="text-2xl font-semibold lg:text-4xl">
                   Senior UI/ UX Designer <span>at</span>{" "}
                   <span className="capitalize">Google</span>
                 </p>
-                <p className="mx-auto max-w-xl md:mx-0">
+                <p className="mx-auto max-w-xl text-sm md:mx-0 lg:text-base">
                   We are a global technology leader dedicated to organizing and
                   making the world's information universally accessible.
                 </p>
               </div>
-              <p className="text-md flex w-fit gap-2 rounded-md bg-amber-100 px-2 py-1 text-left font-medium text-amber-500 lg:items-center">
-                <div>
-                  <ExclamationTriangleIcon className="mt-1 h-5 w-5 lg:mt-0" />
-                </div>
-                Job temporarily on hold. Save this opportunity for future
-                updates.
-              </p>
+              <div className="flex justify-center md:justify-start">
+                <p className="text-md flex w-fit gap-2 rounded-md bg-amber-100 px-2 py-1 text-left font-medium text-amber-500 lg:items-center">
+                  <div>
+                    <ExclamationTriangleIcon className="mt-0.5 h-3 w-3 lg:mt-0" />
+                  </div>
+                  <p className="text-xs">
+                    Job temporarily on hold. Save this opportunity for future
+                    updates.
+                  </p>
+                </p>
+              </div>
 
               <div className="flex items-center justify-center gap-2 md:justify-start">
                 <Button variant="outline" className="hover:bg-white">
