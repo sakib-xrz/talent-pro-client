777570e39e23de89326ad2a574d19adb412e836d
sakib-repliq <sakibul.islam@repliq.dev>
2024-01-05 18:17:32 +0600

    card padding fix for small device

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card p-6 text-card-foreground shadow-sm",
+      "rounded-lg border bg-card p-4 text-card-foreground shadow-sm lg:p-6",
       className,
     )}
     {...props}
