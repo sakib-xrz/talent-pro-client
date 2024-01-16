4e4a7917815b092d9b458db5362f07f8ec286cfd
sakib-repliq <sakibul.islam@repliq.dev>
2024-01-16 13:32:23 +0600

    job details skeleton added

--- app/candidate/(without-sidebar)/find-jobs/[id]/components/skeleton/JobDetailsSkeletonPage.jsx
+++ app/candidate/(without-sidebar)/find-jobs/[id]/components/skeleton/JobDetailsSkeletonPage.jsx
@@ -2,6 +2,7 @@ import { Card } from "@/components/ui/card";
 import BannerSectionSkeleton from "./BannerSectionSkeleton";
 import JobCardSectionSkeleton from "./JobCardSectionSkeleton";
 import JobInfoSectionSkeleton from "./JobInfoSectionSkeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function JobDetailsSkeletonPage() {
   return (
@@ -17,6 +18,16 @@ export default function JobDetailsSkeletonPage() {
               <hr />
               <JobInfoSectionSkeleton />
               <hr />
+              <div className="space-y-2">
+                <Skeleton className="h-5 w-40" />
+                <div className="space-y-2">
+                  <Skeleton className="h-2 w-full" />
+                  <Skeleton className="h-2 w-full" />
+                  <Skeleton className="h-2 w-full" />
+                  <Skeleton className="h-2 w-full" />
+                  <Skeleton className="h-2 w-7/12" />
+                </div>
+              </div>
             </Card>
           </div>
         </div>
