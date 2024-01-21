0122b1febcc4541b3e6e8b0d1da47d7f621951fc
sakib-repliq <sakibul.islam@repliq.dev>
2024-01-21 11:17:02 +0600

    remove @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,9 +80,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
