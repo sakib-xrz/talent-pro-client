c918ecef1dfb0884c86381a5a125dd123fc8732a
sakib-xrz <sakibxrz21@gmail.com>
2024-02-26 19:50:19 +0600

    modal added

--- app/recruiter/jobs/[id]/components/Action.jsx
+++ app/recruiter/jobs/[id]/components/Action.jsx
@@ -12,6 +12,19 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
 export default function Action({ job }) {
   return (
     <div>
@@ -51,12 +64,33 @@ export default function Action({ job }) {
               </div>
             </Menu.Item>
             <Menu.Item>
-              <div className="flex w-full cursor-pointer items-center gap-2 bg-white p-4 text-center text-sm font-medium text-primary duration-300 first:rounded-t-sm last:rounded-b-sm hover:bg-destructive hover:text-white">
-                <div>
-                  <TrashIcon className="h-4 w-4" />
-                </div>
-                <p>Delete Job</p>
-              </div>
+              <AlertDialog className="mx-auto w-10/12">
+                <AlertDialogTrigger className="w-1/2 sm:w-full">
+                  <div className="flex w-full cursor-pointer items-center gap-2 bg-white p-4 text-center text-sm font-medium text-primary duration-300 first:rounded-t-sm last:rounded-b-sm hover:bg-destructive hover:text-white">
+                    <div>
+                      <TrashIcon className="h-4 w-4" />
+                    </div>
+                    <p>Delete Job</p>
+                  </div>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone and permanently delete this
+                      experience from our database.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction className="bg-destructive text-destructive-foreground hover:bg-destructive/90">
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </Menu.Item>
           </Menu.Items>
         </Transition>
