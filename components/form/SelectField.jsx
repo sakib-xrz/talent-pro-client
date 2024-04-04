07855d6cb30618b32885f657bd0a7914b28bed3b
sakib-xrz <sakibxrz21@gmail.com>
2024-04-04 20:44:20 +0600

    Remove unnecessary margin-bottom in SelectField label

--- components/form/SelectField.jsx
+++ components/form/SelectField.jsx
@@ -20,7 +20,7 @@ export default function SelectField({
     <div className="flex flex-col">
       <label
         htmlFor={name}
-        className="mb-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
       >
         {label}
       </label>
